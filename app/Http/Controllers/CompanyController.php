bfab3985995d83de7af95c1641e274f83cf21d5d
muhammad jabir <jabirmuhammad126@gmail.com>
2023-05-08 11:46:34 +0500

    changes in request class and try catch

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -15,14 +15,6 @@ use Yajra\Datatables\Datatables;
 class CompanyController extends Controller
 {
 
-    // public function __construct()
-    // {
-    //     $this->middleware(
-    //         'permission: create companies|edit companies| view companies|delete companies',
-    //         ['only' => ['index', 'store', 'create', 'edit', 'show', 'update', 'delete']]
-    //     );
-    // }
-
     public function index(Request  $request)
     {
         $company = Company::all();
@@ -64,11 +56,11 @@ class CompanyController extends Controller
                 $request->validate();
             }
             Company::create($data);
-        if($request->user()->hasRole('admin')){
-             $user=Auth::user();
-            \App\Jobs\MailJob::dispatch($user)->delay(now()->addSecond(1));
-        }
-          
+            if ($request->user()->hasRole('admin')) {
+                $user = Auth::user();
+                \App\Jobs\MailJob::dispatch($user)->delay(now()->addSecond(1));
+            }
+
             DB::commit();
             return response([
                 'company ' => ' company  is created'
@@ -102,22 +94,28 @@ class CompanyController extends Controller
      * Update the specified resource in storage.
      */
     // Inside the update method of the controller
-    public function update(Request $request, Company $company)
+    public function update(CompanyRequest $request, Company $company)
     {
-        $company->name = $request->input('name');
-        $company->email = $request->input('email');
-        if ($request->hasFile('image')) {
-            Storage::delete('public/images/' . $company->logo);
-            $image = $request->file('image');
-            $filename = $image->getClientOriginalName();
-            $path = $request->file('image')->storeAs('public/images', $filename);
-            $company->logo = $filename;
+
+        try {
+            if ($request->hasFile('image')) {
+                Storage::delete('public/images/' . $company->logo);
+                $image = $request->file('image');
+                $filename = $image->getClientOriginalName();
+                $path = $request->file('image')->storeAs('public/images', $filename);
+                $company->logo = $filename;
+            }
+            $company->update($request->all());
+            return response()->json([
+                'success' => true,
+                'message' => 'Company updated successfully.'
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+
+            ], 401);
         }
-        $company->update();
-        return response()->json([
-            'success' => true,
-            'message' => 'Company updated successfully.'
-        ]);
     }
 
     public function destroy(Company $company)
@@ -130,19 +128,12 @@ class CompanyController extends Controller
     {
         $search = request('search');
         $projects = Project::Where('detail', 'like', "%$search%")->pluck('detail');
-        
+
         $companies = Company::where(function ($query) use ($projects) {
             foreach ($projects  as $project) {
                 $query->orWhere('name', 'like', "%$project%");
             }
         })->get();
-        // projects search by name
-        // $companies= Company::Where('name', 'like', "%$search%")->pluck('name','email');
-        // $companies = Project::where(function ($query) use ($companies) {
-        //     foreach ($companies as $project) {
-        //         $query->orWhere('detail', 'like', "%$project%");
-        //     }
-        // })->get();
         return view('companies.searching', compact('companies'));
     }
 }
